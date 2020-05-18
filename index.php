fec97311552039da55642f374b402529e6aaf236
tanakadamon <kizan246@gmail.com>
2020-05-18 17:37:30 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Tanaka Hideto">
+        <title>PH34 サンプル１|エンティティ</title>
+    </head>
+    <body>
+        <h1>エンティティ</h1>
+            <ol>
+                <li><a href="/ph34/entity/findOnewithountEntity.php">エンティティを使わずに１行のデータ取得</a></li>
+                <li><a href="/ph34/entity/findone/findOnewithountEntity.php">エンティティを使って１行のデータ取得</a></li>
+                <li><a href="/ph34/entity/findmany/findOnewithountEntity.php">エンティティを使わずに１行のデータ取得</a></li>
+            </ol>
+    </body>
+</html>
